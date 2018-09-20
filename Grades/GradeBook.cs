51c9c40d6ec0a78de6b3ea21370644fde1796006
Carl du Preez <cdupreez@astutefse.com>
2018-09-20 16:00:19 +0200

    grade book first commit

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        private List<float> grades;
+
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+        
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        public float GetAverage()
+        {
+            return grades.Average();
+        }
+
+        public float GetMax()
+        {
+            return grades.Max();
+        }
+
+        public float GetMin()
+        {
+            return grades.Min();
+        }
+    }
+}
---
+++ Grades/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Grades;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook gradeBook = new GradeBook();
+
+            Console.WriteLine("Enter Grade, 0 to stop");
+            string answer = Console.ReadLine();
+
+            while (answer != "0")
+            {
+                try
+                {
+                    gradeBook.AddGrade(float.Parse(answer));
+                }
+                catch (Exception errorException)
+                {
+                    Console.WriteLine(errorException);
+                }
+
+                Console.WriteLine("Enter Grade, 0 to stop");
+                answer = Console.ReadLine();
+            }
+
+
+            Console.WriteLine("Max: " + gradeBook.GetMax());
+            Console.WriteLine("Min: " + gradeBook.GetMin());
+
+            Console.WriteLine("Average: " + gradeBook.GetAverage());
+            Console.ReadKey();
+        }
+    }
+}
